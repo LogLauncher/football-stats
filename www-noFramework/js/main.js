4665cb2fc801eb3c1c31603c159c69b80f656f58
LogLauncher <struanforsyth@gmail.com>
2018-03-06 15:07:04 +0100

    Modified the system to use an object and not 2 arrays

--- www-noFramework/js/main.js
+++ www-noFramework/js/main.js
@@ -4,38 +4,68 @@ $(document).ready(function() {
         dynamicTyping: true,
         skipEmptyLines: true
     })["data"];
-    
-    console.log("Data", Object.keys(data[0]))
-    let labels = Object.keys(data[0]);
-    let teams = []
-    let scores = []
-    data.forEach(row => {
-        if(teams.indexOf(row.home_team) == -1) {
-            teams.push(row.home_team);
-            scores.push(0);
-        }
-        if(teams.indexOf(row.away_team) == -1) {
-            teams.push(row.away_team);
-            scores.push(0);            
-        }
-        let index = teams.indexOf(row.home_team);
-        scores[index] += row.home_score;
-        index = teams.indexOf(row.away_team);
-        scores[index] += row.away_score;
-    })  
 
-    console.log(teams);
-    console.log(scores);
+    console.log("CSV data after papaparse", data);
+    
+    
+    let goalsPerTeam = []
+    data.forEach(row => {
+        let homeTeamName = row.home_team
+        let awayTeamName = row.away_team
+        if(!containesTeam(goalsPerTeam, homeTeamName)) {
+            goalsPerTeam.push(addNewTeam(homeTeamName))
+        }
+        if(!containesTeam(goalsPerTeam, awayTeamName)) {
+            goalsPerTeam.push(addNewTeam(awayTeamName))         
+        }
+
+        addScore(goalsPerTeam, homeTeamName, row.home_score)
+        addScore(goalsPerTeam, awayTeamName, row.away_score)
+    })
 
     let ctx = $('#myChart');
     let myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: teams,
+            labels: getTeamNames(goalsPerTeam),
             datasets: [{
                 label: 'total goals',
-                data: scores
+                data: getTeamScores(goalsPerTeam)
             }]
         }
     })
 })
+
+function getTeamNames(teams) {
+    let names = []
+    teams.forEach(team => {
+        names.push(team.name)
+    })
+    return names
+}
+
+function getTeamScores(teams) {
+    let scores = []
+    teams.forEach(team => {
+        scores.push(team.score)
+    })
+    return scores
+}
+
+function containesTeam(teams, name) {
+    let team = $.grep(teams, function(team) {
+        return team.name == name
+    })
+    return team.length > 0
+}
+
+function addNewTeam(name) {
+    return {name: name, score: 0}
+}
+
+function addScore(teams, name, score) {
+    let team = ($.grep(teams, function(team) {
+        return team.name == name
+    }))[0]
+    team.score += score
+}
